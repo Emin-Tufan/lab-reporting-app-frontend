40d99813f8f5ef47d9ecaad0f1b6611343fe3d53
Emin <eminttufan@gmail.com>
2024-01-14 05:20:27 +0300

    DockerFile Added

--- src/layouts/ProtectedRoute/ProtectedRoute.tsx
+++ src/layouts/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Outlet, Route } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../Auth/UseAuth';
 import { jwtDecode } from 'jwt-decode';
 
