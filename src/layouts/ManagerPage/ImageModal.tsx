6a3c104f1c098a77b082b1963e7070088e54a282
Emin <eminttufan@gmail.com>
2024-01-17 06:59:10 +0300

    Modal image update

--- src/layouts/ManagerPage/ImageModal.tsx
+++ src/layouts/ManagerPage/ImageModal.tsx
@@ -2,7 +2,7 @@ const ImageModal: React.FC<({ image: string, id: number })> = (props) => {
 
     return (
         <div className="modal fade" id={`showImage${props.id}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div className="modal-dialog">
+            <div className="modal-dialog modal-dialog-centered modal-xl">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="staticBackdropLabel">Report Image</h1>
